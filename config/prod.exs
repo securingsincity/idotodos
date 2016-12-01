a8fca772110577a7fd3cd265f5574da8aed67d5e
James Hrisho <james.hrisho@gmail.com>
2016-11-30 20:54:39 -0500

    Configuration

--- config/prod.exs
+++ config/prod.exs
@@ -13,12 +13,29 @@ use Mix.Config
 # which you typically run after static files are built.
 config :idotodos_ex, IdotodosEx.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
-
+  url: [scheme: "https", host: "idotodos.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 # Do not print debug messages in production
 config :logger, level: :info
 
+
+
+config :guardian, Guardian,
+  allowed_algos: ["HS512"], # optional
+  verify_module: Guardian.JWT,  # optional
+  issuer: "IdotodosEx",
+  ttl: { 30, :days },
+  verify_issuer: true, # optional
+  secret_key: System.get_env("GUARDIAN_SECRET_KEY"), #overwrite in prod.secrets
+  serializer: IdotodosEx.GuardianSerializer
+
+config :idotodos_ex, IdotodosEx.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
@@ -55,6 +72,8 @@ config :logger, level: :info
 #
 #     config :idotodos_ex, IdotodosEx.Endpoint, server: true
 #
+
+
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.

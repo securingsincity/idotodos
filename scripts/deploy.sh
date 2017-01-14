fe4db5cb5c1508091207133b863fb482bc8a4161
James Hrisho <james.hrisho@gmail.com>
2017-01-14 13:36:14 -0500

    deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
+export IS_TAG=$([[ ! -z "$TRAVIS_TAG" ]] && echo "1" || echo "0")
 
 if [[ $IS_TAG -gt 0 ]]; then
 
   echo "Preparing to deploy";
   mkdir build
   mv * build
-  tar -czf package.tgz build
-  scp package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+  tar -czf $TRAVIS_TAG.tgz build
+  scp $TRAVIS_TAG.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
 #   ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/deploy.sh
 fi

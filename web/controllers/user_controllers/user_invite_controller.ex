c378b0714d6342c6bd786b454f7dc7ab85a72d01
James Hrisho <james.hrisho@gmail.com>
2017-01-15 22:28:50 -0500

    send to some parties

--- web/controllers/user_controllers/user_invite_controller.ex
+++ web/controllers/user_controllers/user_invite_controller.ex
@@ -2,6 +2,7 @@ defmodule IdotodosEx.UserInviteController do
     use IdotodosEx.Web, :controller
     alias IdotodosEx.Invite
     alias IdotodosEx.Guest
+    alias IdotodosEx.Party
     def index(conn, _params) do
         campaign_id = Guardian.Plug.current_resource(conn).campaign_id
         invites = Repo.all(
@@ -57,27 +58,38 @@ defmodule IdotodosEx.UserInviteController do
         campaign_id = Guardian.Plug.current_resource(conn).campaign_id
         invite = Repo.get_by!(Invite, %{id: id, campaign_id: campaign_id})
         Invite.changeset(invite)
-        render(conn, "send.html", invite: invite)
+        parties = Repo.all(
+           from party in Party,
+           where: [campaign_id: ^campaign_id],
+           select: {party.name, party.id}
+        ) 
+        render(conn, "send.html", invite: invite, parties: parties)
     end
 
-    def send_email(conn, %{"id" => id, "send_invite"=> %{ "who" => who }}) do
+    def send_email(conn, %{"id" => id, "send_invite"=> %{ "who" => who, "parties" => list_of_ids }}) do
         campaign_id = Guardian.Plug.current_resource(conn).campaign_id
         invite = Repo.get_by!(Invite, %{id: id, campaign_id: campaign_id})
         case who do
             "all" -> send_to_all_parties(campaign_id, invite)
+            "some" -> send_to_some_parties(list_of_ids, campaign_id, invite)
         end
         conn
             |> put_flash(:info, "Invites sent successfully")
             |> redirect(to: user_invite_path(conn, :index))        
     end
 
-    def send_to_all_parties(campaign_id, invite) do 
+    def send_to_some_parties(list_of_ids, campaign_id, invite) do
         Repo.all(
            from guest in Guest,
-           where: [campaign_id: ^campaign_id]
+           where: guest.party_id in ^list_of_ids
         )  
         |> Repo.preload(:party)
-        |> Enum.each(fn guest -> 
+        |> send_to_guests(campaign_id, invite)
+    end
+
+    def send_to_guests(guests, campaign_id, invite) do
+       guests
+       |> Enum.each(fn guest -> 
             if guest.email !== "" && guest.email !== nil do
               formatted_email = format_email(invite.html, invite.subject, guest)
               formatted_text = format_email(invite.email_text, invite.subject, guest)
@@ -103,6 +115,14 @@ defmodule IdotodosEx.UserInviteController do
               end
             end 
         end)
+    end
+    def send_to_all_parties(campaign_id, invite) do 
+        Repo.all(
+           from guest in Guest,
+           where: [campaign_id: ^campaign_id]
+        )  
+        |> Repo.preload(:party)
+        |> send_to_guests(campaign_id, invite)
     end
 
     def format_email(html, subject, user) do

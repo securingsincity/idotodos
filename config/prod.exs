be6927839a70ee16c65f9c483efeaca51210bfb1
James Hrisho <james.hrisho@gmail.com>
2017-02-11 18:23:58 -0500

    Fix prod config

--- config/prod.exs
+++ config/prod.exs
@@ -35,7 +35,7 @@ config :idotodos_ex, IdotodosEx.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true
+  ssl: true,
   loggers: [{Ecto.LogEntry, :log, []}, {IdotodosEx.RepoMetrics, :record_metric, []}]
 
 config :ex_statsd,

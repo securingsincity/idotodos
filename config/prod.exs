47b25f4a8a97f1fc2a32b2962981e3de061d719b
James Hrisho <james.hrisho@gmail.com>
2017-02-20 16:06:31 -0500

    dont' force ssl

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :idotodos_ex, IdotodosEx.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: System.get_env("HEROKU_URL"), port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  force_ssl: false,
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 # Do not print debug messages in production

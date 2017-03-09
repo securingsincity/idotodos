2115a3c7c86117addef351b342d22b34c590dd0c
James Hrisho <james.hrisho@gmail.com>
2017-03-08 21:02:01 -0500

    Adds a bunch of formatting functionality

--- web/views/wedding_view.ex
+++ web/views/wedding_view.ex
@@ -1,20 +1,30 @@
 defmodule IdotodosEx.WeddingView do
   use IdotodosEx.Web, :view
 
-  def responsive_bubbles(list, max_columns, partial) do
+  def responsive_bubbles(conn, list, max_columns, partial) do
     total_width = 12
     list_count = Enum.count(list)
     list_count_less_than_columns = list_count < max_columns
-    if list_count || max_columns == 0 do
+    if list_count == 0 || max_columns == 0 do
       nil
     else
       column_width = case list_count_less_than_columns do
         true -> round(total_width / list_count)
         false -> round(total_width / max_columns)
       end
-      Enum.map(list, fn(item) ->
-        render IdotodosEx.WeddingView, partial <> ".html", column_width: column_width, item: item
+      list
+      |> Enum.with_index
+      |> Enum.map(fn({item, index}) ->
+        render IdotodosEx.WeddingView, partial <> ".html", conn: conn, column_width: column_width, item: item, index: index, max_columns: max_columns,list_count: list_count
       end)
+    end
+  end
+
+  def registry_image(conn, name) do
+    case name do
+      "Honeyfund" -> static_path(conn, "/images/honeyfund.png")
+      "Crate and Barrel" -> static_path(conn, "/images/crateandbarrel.png")
+      "Amazon" -> static_path(conn, "/images/amazon.png")
     end
   end
 

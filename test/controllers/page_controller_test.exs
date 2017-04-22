d28eba4b2a608c4eed795cfd85cb8da6bc6afea2
James Hrisho <james.hrisho@gmail.com>
2017-04-22 08:47:55 -0400

    These tests man

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -5,11 +5,4 @@ defmodule IdotodosEx.PageControllerTest do
     assert html_response(conn, 200) =~ "For Humans"
   end
 
-
-  test "GET /app", %{conn: conn} do
-    conn = get conn, "/app"
-    assert html_response(conn, 200) =~ "Welcome James! your wedding is in 1 year"
-  end
-
-
 end
